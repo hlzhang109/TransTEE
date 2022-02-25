163a4974da938305b3687a1c46b2710fa851d637
Yi-Fan Zhang <yifanzhang.cs@gmail.com>
2022-02-25 22:21:52 +0800

    Delete Structured directory

--- Structured/cal_result.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-x_in = [  8.100875068441491, ]
-x_out = [9.90053726286115, ] 
-
-print("{:.2f} ± {:.2f}".format(np.mean(x_in), np.std(x_in)))
-print("{:.2f} ± {:.2f}".format(np.mean(x_out), np.std(x_out)))
--- Structured/configs/utils.py
+++
@@ -1,2 +0,0 @@
-def str2bool(v):
-    return v.lower() == "true"
